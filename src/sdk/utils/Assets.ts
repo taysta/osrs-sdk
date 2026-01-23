cd3e247bead9d388b8d3cec9e028f7e29ba7f4b1
taysta <taylor.brough13@gmail.com>
2026-01-23 20:13:44 +0800

    - change cdn url for homelab

--- src/sdk/utils/Assets.ts
+++ src/sdk/utils/Assets.ts
@@ -9,9 +9,9 @@ export class Assets {
    * Returns the appropriate URL for an asset and also schedules it for preloading.
    */
   static getAssetUrl(asset: string) {
-    // TODO switch CDN based on build variable 
+    // TODO switch CDN based on build variable
     //const url = `https://assets-soltrainer.netlify.app/${asset}`;
-    const url = `https://oldschool-cdn.com/${asset}`;
+    const url = `https://inferno.tayst.au/${asset}`;
     if (Assets.loadedAssets[url]) {
       return url;
     }
